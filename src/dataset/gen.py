5b77dbc4dcab62c1c8933194fa967fb99204642c
cli <82491956+xa1on@users.noreply.github.com>
2026-01-03 14:30:20 -0800

    docs: docstring w/ todo for dataset gen

---
+++ src/dataset/gen.py
@@ -0,0 +1,14 @@
+"""
+Generate architectural datasets for symbol detection
+
+TODO:
+1. grab images from architectural pdfs
+2. add symbols randomly to architectural pdfs, resembling real architectural documents.
+    - hough line transform to detect walls/pipes.
+    - strategically place T symbols (thermostats) along walls
+    - place bowties (valves??) on pipelines
+    - Keynotes: probably just good to put them randomly
+3. Generate in YOLO format
+
+- Chenghao Li
+"""
